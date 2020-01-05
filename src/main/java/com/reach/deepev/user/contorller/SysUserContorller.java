138303fc6ac350d5b1928aaee63d8e71237b29a2
xjzh1986 <273904882@163.com>
2020-01-05 16:54:50 +0800

    测试

--- src/main/java/com/reach/deepev/user/contorller/SysUserContorller.java
+++ src/main/java/com/reach/deepev/user/contorller/SysUserContorller.java
@@ -20,7 +20,7 @@ public class SysUserContorller {
     public SysUser index(@RequestBody SysUser sysUser) {
         System.out.println(sysUser.getUserName());
         System.out.println(sysUser.getPassword());
-
+        System.out.println(sysUser.getPassword());
         return sysUser;
     }
 }
